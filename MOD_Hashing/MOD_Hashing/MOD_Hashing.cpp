4b8b78dd1f9f83438df605c4f803cc86df4dea05
Andrew Maynez <andysgomez@hotmail.com>
2019-05-31 09:23:23 -0600

    added readline function for cin

--- MOD_Hashing/MOD_Hashing/MOD_Hashing.cpp
+++ MOD_Hashing/MOD_Hashing/MOD_Hashing.cpp
@@ -2,10 +2,23 @@
 #include <string>
 using namespace std;
 
+void Split() {
+}
+
+void Hash() {
+
+}
+
 int main()
 {
-	cout << "hi";
+	string input;
 
-	system("pause");
+	cout << "Enter words to be hashed: ";
+	getline(cin, input);
+
+	cout << input[1] << endl;
+	cout << input[2] << endl;
+
+	//system("pause");
 	return 0;
 } // END MAIN
