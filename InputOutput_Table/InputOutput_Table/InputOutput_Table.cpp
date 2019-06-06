3a7d1000e499053c8c306837987349fa13b4fd10
Andrew Maynez <andysgomez@hotmail.com>
2019-06-06 17:50:51 -0600

    All functions completed.  Just need to unit test.

--- InputOutput_Table/InputOutput_Table/InputOutput_Table.cpp
+++ InputOutput_Table/InputOutput_Table/InputOutput_Table.cpp
@@ -3,6 +3,8 @@ using namespace std;
 
 // Declarations
 void Toggle(bool& _var, int& _counter, int _terminal);
+bool Function(bool _a, bool _b, bool _c, bool _d, bool _e);
+bool Negation(bool _x);
 
 // Begin MAIN
 int main()
@@ -11,9 +13,13 @@ int main()
 	bool a = true, b = true, c = true, d = true, e = true;
 	int a_counter = 1, b_counter = 1, c_counter = 1, d_counter = 1, e_counter = 1;
 
+	// Display column headers
+	cout << "a b c d e | (( c + ~d ) * b ) * ~( d + a * e )\n-----------------------------------------------" << endl;
+
 	// Repeat 32 times
 	for (int i = 1; i <= 32; i++)
 	{
+		// Call Toggle()
 		Toggle(a, a_counter, 17);
 		Toggle(b, b_counter, 9);
 		Toggle(c, c_counter, 5);
@@ -21,10 +27,11 @@ int main()
 		Toggle(e, e_counter, 2);
 
 		// Increment all counter variables
-		a_counter++, b_counter, c_counter++, d_counter++, e_counter++;
+		a_counter++, b_counter++, c_counter++, d_counter++, e_counter++;
 
-		cout << i << " " << a << " " << b << " " << c << " " << d << " " << e << endl;
+		cout << a << " " << b << " " << c << " " << d << " " << e << "\t" << Function(a, b, c, d, e) << endl;
 	}// END FOR-LOOP
+
 	return 0;
 }// End MAIN
 
@@ -44,6 +51,18 @@ void Toggle(bool& _var, int& _counter, int _toggle) {
 	}
 }// END TOGGLE METHOD
 
-// AND METHOD
-// OR METHOD
-// NEGATION METHOD
+// Function METHOD
+bool Function(bool _a, bool _b, bool _c, bool _d, bool _e) {
+	// (( c + ~d ) * b ) * ~( d + a * e )
+	return ((_c + Negation(_d)) * _b) * Negation((_d + _a * _e));
+}
+
+// Negation Method
+bool Negation(bool _x) {
+	if (_x == true) {
+		return false;
+	}
+	else if (_x == false) {
+		return true;
+	}
+}// END NEGATION METHOD
