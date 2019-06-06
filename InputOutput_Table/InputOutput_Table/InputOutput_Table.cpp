d7723923945a1c25f811969af97b71543d94a156
Andrew Maynez <andysgomez@hotmail.com>
2019-06-06 16:07:36 -0600

    removed vommented out code

--- InputOutput_Table/InputOutput_Table/InputOutput_Table.cpp
+++ InputOutput_Table/InputOutput_Table/InputOutput_Table.cpp
@@ -7,89 +7,21 @@ void Toggle(bool& _var, int& _counter, int _terminal);
 // Begin MAIN
 int main()
 {
-	bool a = true;
-	bool b = true;
-	bool c = true;
-	bool d = true;
-	bool e = true;
+	// Declare variables
+	bool a = true, b = true, c = true, d = true, e = true;
+	int a_counter = 1, b_counter = 1, c_counter = 1, d_counter = 1, e_counter = 1;
 
-	int a_counter = 1;
-	int b_counter = 1;
-	int c_counter = 1;
-	int d_counter = 1;
-	int e_counter = 1;
-
+	// Repeat 32 times
 	for (int i = 1; i <= 32; i++)
 	{
-		/*
-		// Variable 'a' toggle
-		if (a_counter == 17 && a == true) {
-			a_counter = 1;
-			a = false;
-		}
-		else if (a_counter == 17 && a == false) {
-			a_counter = 1;
-			a = true;
-		}
-
-		// Variable 'b' toggle
-		if (b_counter == 9 && b == true) {
-			b_counter = 1;
-			b = false;
-		}
-		else if (b_counter == 9 && b == false) {
-			b_counter = 1;
-			b = true;
-		}
-
-		// Variable 'c' toggle
-		if (c_counter == 2) {
-			if (c == true) {
-				c = false;
-			}
-			else if (c == false) {
-				c = true;
-			}
-			// Reset variable's counter
-			c_counter = 1;
-		}
-
-		// Variable 'd' toggle
-		if (d_counter == 3) {
-			if (d == true) {
-				d = false;
-			}
-			else if (d == false) {
-				d = true;
-			}
-			// Reset variable's counter
-			d_counter = 1;
-		}
-
-		// Variable 'e' toggle
-		if (e_counter == 2) {
-			if (e == true) {
-				e = false;
-			}
-			else if (e == false) {
-				e = true;
-			}
-			// Reset variable's counter
-			e_counter = 1;
-		}
-		*/
-
 		Toggle(a, a_counter, 17);
 		Toggle(b, b_counter, 9);
 		Toggle(c, c_counter, 5);
 		Toggle(d, d_counter, 3);
 		Toggle(e, e_counter, 2);
 
-		a_counter++;
-		b_counter++;
-		c_counter++;
-		d_counter++;
-		e_counter++;
+		// Increment all counter variables
+		a_counter++, b_counter, c_counter++, d_counter++, e_counter++;
 
 		cout << i << " " << a << " " << b << " " << c << " " << d << " " << e << endl;
 	}// END FOR-LOOP
@@ -111,3 +43,7 @@ void Toggle(bool& _var, int& _counter, int _toggle) {
 		_counter = 1;
 	}
 }// END TOGGLE METHOD
+
+// AND METHOD
+// OR METHOD
+// NEGATION METHOD
