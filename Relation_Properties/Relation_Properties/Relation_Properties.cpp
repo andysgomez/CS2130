6bb83f2fe336b280ddd5e397d7f99afb3779cbfc
Andrew Maynez <andysgomez@hotmail.com>
2019-06-20 16:33:49 -0600

    first commit relation_properties

---
+++ Relation_Properties/Relation_Properties/Relation_Properties.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+
+int main()
+{
+	int m1[4][4] =
+	{
+		{1,0,0,0},
+		{0,1,0,0},
+		{0,0,1,0},
+		{0,0,0,1}
+	};
+
+	int m2[4][4] =
+	{
+		{0,0,0,1},
+		{0,0,1,0},
+		{0,1,0,0},
+		{1,0,0,0}
+	};
+
+	int m3[4][4] =
+	{
+		{1,0,0,0},
+		{1,0,0,0},
+		{1,0,0,0},
+		{1,0,0,0}
+	};
+
+	int m4[4][4] =
+	{
+		{0,0,0,0},
+		{0,0,0,0},
+		{0,0,0,0},
+		{0,0,0,0}
+	};
+
+	std::cout << "hi" << std::endl;
+
+	system("pause");
+	return 0;
+}
+
+// Reflexivity
+
+// Symmetry
+
+// Reflexive
+void Reflexive() {
+
+}
+
+// Anti-Reflexive
+void AntiReflexive() {
+
+}
+
+// Symmetric
+void Symmetric() {
+
+}
+
+// Anti-Symmetric
+void AntiSymmetric() {
+
+}
+
+// Asymmetric
+void Asymmetric() {
+
+}
+
+/*
+Reflexive = All 1s on its main diagonal.
+Anti-Reflexive = All 0s on its main diagonal.
+*/
+
+/*
+Symmetric matrix = squre matrix nxn equals transposed
+Antisymmetric
+Asymmetric
+*/
